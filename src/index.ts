8498a704758b337996beb242721a75c25c81893c
Ardi Jorganxhi <ardijorganxhi@eskisehir.edu.tr>
2024-03-28 01:13:48 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+    res.send("Hello, TypeScript with Express!");
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port:' + port)
+})
